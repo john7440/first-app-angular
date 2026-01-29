2ba935365403cbadc270d176d31aac94de4916e9
Jonathan M. <neodev7440@gmail.com>
2026-01-29 09:59:43 +0100

    ajout de la route pour la page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { TrainingsComponent } from './components/trainings/trainings';   
 import { CartComponent } from './components/cart/cart';
 import { CheckoutComponent } from './components/checkout/checkout';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'trainings' },
+  { path: '', pathMatch: 'full', redirectTo: 'trainings' },
   { path: 'trainings', component: TrainingsComponent },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
-  { path: '**', redirectTo: 'trainings' }
+  { path: '**', component: PageNotFoundComponent },
+  
 ];
