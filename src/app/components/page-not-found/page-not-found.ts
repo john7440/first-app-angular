6b982f6d35c71d91a67905c27d3280151ea9c6c9
Jonathan M. <neodev7440@gmail.com>
2026-01-29 09:55:44 +0100

    ajout du fichier ts de la page 404

---
+++ src/app/components/page-not-found/page-not-found.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './page-not-found.html',
+  styleUrl: './page-not-found.css',
+})
+
+export class PageNotFoundComponent {
+
+}
