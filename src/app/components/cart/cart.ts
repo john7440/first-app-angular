618b54dd851ad10f34719cfa8775f64001d930b7
Jonathan M. <neodev7440@gmail.com>
2026-01-29 13:35:27 +0100

    ajout des commentaire au propre

--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -4,6 +4,9 @@ import { CartService } from '../../services/cart';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
+/**
+ * affiche les articles, permet de modifier les quantités et de payer
+ */
 @Component({
     selector: 'app-cart',
     standalone: true,
@@ -15,8 +18,15 @@ import { Router } from '@angular/router';
 export class CartComponent {
     readonly cart = inject(CartService);
     private readonly router = inject(Router);
+
+    //signal pour la recherche des articles dans le panier
     cartQuery = signal('');
 
+    /**
+     * Calcule la liste des articles filtrés selon la recherche
+     * Recherche dans le nom et la description des formations
+     * Retourne tous les articles si la recherche est vide
+     */
     readonly filteredItems = computed(() => {
     const q = this.cartQuery().trim().toLowerCase();
     const items = this.cart.items();
@@ -28,16 +38,25 @@ export class CartComponent {
     );
   });
 
+  /**
+     * Supprime un article du panier
+     * @param id Id de la formation à supprimer
+     */
     remove(id:number) {this.cart.remove(id); }
 
+    //vider panier
     clear() { this.cart.clear(); }
 
+    //incrémentation
     increase(id: number) { this.cart.increase(id); }
 
+    //décrémentation
     decrease(id: number) { this.cart.decrease(id); }
 
+    //mettre a jour la quantité
     update(id: number, qty: number) { this.cart.updateQuantity(id, qty); }
 
+    // redirige vers la page de validation de la commande
     checkout() {
         this.router.navigate(['/checkout']);
     }
