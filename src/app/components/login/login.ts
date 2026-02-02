a3982f5ff96d8916a825765ceb4c2379b5bd4b73
Jonathan M. <neodev7440@gmail.com>
2026-02-02 14:12:30 +0100

    ajout des injections nécessaire au loginComponenet

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
 
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  loginForm = FormGroup;
 }
