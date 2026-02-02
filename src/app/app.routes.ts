3b2cf9a7f0dd277c62a6110448bedaa578bacd6c
Jonathan M. <neodev7440@gmail.com>
2026-02-02 14:31:04 +0100

    ajout de la route pour le login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { TrainingsComponent } from './components/trainings/trainings';
 import { CartComponent } from './components/cart/cart';
 import { CheckoutComponent } from './components/checkout/checkout';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found';
+import { LoginComponent } from './components/login/login';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'trainings' },
   { path: 'trainings', component: TrainingsComponent },
+  { path: 'login', component: LoginComponent},
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
-  { path: '**', component: PageNotFoundComponent },
-  
+  { path: '**', component: PageNotFoundComponent }, 
 ];
