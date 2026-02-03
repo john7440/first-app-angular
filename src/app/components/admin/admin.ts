b2c3513ad5c435b68e9e972a722c9e16c56e8043
Jonathan M. <neodev7440@gmail.com>
2026-02-03 13:54:08 +0100

    ajout des signaux + constructeur du formulaire admin

--- src/app/components/admin/admin.ts
+++ src/app/components/admin/admin.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TrainingService } from '../../services/training.service';
+import { Training } from '../../model/training/training';
+import { validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +10,30 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {
+export class AdminComponent {
+  private readonly fb = inject(FormBuilder);
+  private readonly trainingService = inject(TrainingService);
+
+  trainings = signal<Training[]>([]);
+  isLoading = signal(false);
+  errorMsg = signal<string | null>(null);
+  successMsg = signal<string | null>(null);
+
+  editMode = signal(false);
+  editingId = signal<number | null>(null);
+
+  trainingForm: FormGroup;
+
+  showModal = signal(false)
+
+  constructor() {
+    this.trainingForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3) ]],
+      description: ['',[Validators.required, Validators.minLength(10)]],
+      price: [0,[Validators.required, Validators.min(0)]],
+      category: ['', Validators.required]
+    });
+  }
+  
 
 }
