623d95b97e7eec3c5a43f0e62aa4c7a26abb999f
Jonathan M. <neodev7440@gmail.com>
2026-02-03 15:04:20 +0100

    ajout de la route de l'admin avec guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { CartComponent } from './components/cart/cart';
 import { CheckoutComponent } from './components/checkout/checkout';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found';
 import { LoginComponent } from './components/login/login';
+import { AdminComponent } from './components/admin/admin';
+import { adminGuard } from './guards/admin-guard';
+
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'trainings' },
   { path: 'trainings', component: TrainingsComponent },
   { path: 'login', component: LoginComponent},
+  { path: 'admin', component: AdminComponent, canActivate: [adminGuard]},
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: '**', component: PageNotFoundComponent }, 
