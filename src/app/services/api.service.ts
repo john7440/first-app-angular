86eb47f43137006c22007eb44f6f34e730920d52
Jonathan M. <neodev7440@gmail.com>
2026-02-03 10:34:23 +0100

    ajout d'un service api

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService{
+    private readonly http = inject(HttpClient);
+    private readonly apiUrl = environment.apiUrl;
+
+    getAll<T>(endpoint: string): Observable<T[]>{
+        return this.http.get<T[]>(`${this.apiUrl}/${endpoint}`).pipe(
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(error: HttpErrorResponse){
+        let errorMsg = 'Une erreur est survenue';
+
+        if(error.error instanceof ErrorEvent){
+            errorMsg = 'Erreur: ${error.error.message}';
+        } else {
+            errorMsg = 'Erreur n: ${error.status}\nMessage: ${error.message}';
+        }
+        console.error(errorMsg);
+        return throwError(() => new Error(errorMsg));
+    }
+}
