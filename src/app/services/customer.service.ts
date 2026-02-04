e915fdeb500546500ca839d1796914b9d36b5a84
Jonathan M. <neodev7440@gmail.com>
2026-02-04 10:08:21 +0100

    ajout des fonction get et create

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,26 @@
-import { Injectable} from '@angular/core';
+import { inject, Injectable} from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { Customer } from '../model/customer.model';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class CustomerService {
+    private readonly api = inject(ApiService);
+    private readonly endpoint = 'customers';
+
+    getAllCustomers(): Observable<Customer[]>{
+        return this.api.getAll<Customer>(this.endpoint);
+    }
+
+    getCustomerBytId(id: number): Observable<Customer>{
+        return this.api.getById<Customer>(this.endpoint, id);
+    }
+
+    createCustomer(customer: Omit<Customer, 'id'>): Observable<Customer>{
+        return this.api.create<Customer>(this.endpoint, customer);
+    }
 
 }
