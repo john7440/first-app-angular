200b507a5284705c6179238a64dbfecfd362f8a1
Jonathan M. <neodev7440@gmail.com>
2026-02-04 10:12:08 +0100

    ajout des fonctions update et delete

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable} from '@angular/core';
 import { ApiService } from './api.service';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Customer } from '../model/customer.model';
 
 @Injectable({
@@ -21,6 +21,14 @@ export class CustomerService {
 
     createCustomer(customer: Omit<Customer, 'id'>): Observable<Customer>{
         return this.api.create<Customer>(this.endpoint, customer);
+    }
+
+    updateCustomer(id:number, customer: Partial<Customer>): Observable<Customer> {
+        return this.api.update<Customer>(this.endpoint, id,customer);
+    }
+
+    deleteCustomer(id: number): Observable<void>{
+        return this.api.delete<Customer>(this.endpoint, id).pipe(map(()=> void 0));
     }
 
 }
