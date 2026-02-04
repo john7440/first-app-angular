083fe25b17e06857217f125aced4900947904ffc
Jonathan M. <neodev7440@gmail.com>
2026-02-04 10:19:03 +0100

    ajout d'un service pour les commande avec les fonctions get + create

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { ApiService } from './api.service';
+import { Order } from '../model/customer.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  private readonly api = inject(ApiService);
+  private readonly endpoint = 'orders';
+
+  getAllOrders(): Observable<Order[]>{
+    return this.api.getAll<Order>(this.endpoint);
+  }
+
+  getOrderById(id: number): Observable<Order>{
+    return this.api.getById<Order>(this.endpoint, id);
+  }
+
+  createOrder(order: Omit<Order,'id'>): Observable<Order> {
+    return this.api.create<Order>(this.endpoint, order);
+  }
+
+}
