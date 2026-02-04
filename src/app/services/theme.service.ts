387b06f17e7b2dde7f2af4d99ca1d0dd6f8d8b06
Jonathan M. <neodev7440@gmail.com>
2026-02-04 13:44:18 +0100

    ajout d'un service pour gérer le theme (clair/sombre)

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, signal, effect } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+    theme = signal<'light' | 'dark'>('light');
+
+    constructor(){
+        const savedTheme = localStorage.getItem('theme') as  'light' | 'dark' | null;
+        const prefersDark = window.matchMedia('(prefers-color-scheme: dark').matches;
+
+        this.theme.set(savedTheme || (prefersDark ? 'dark': 'light'));
+
+        effect(() =>{
+            const currentTheme = this.theme();
+            document.documentElement.setAttribute('data-bs-theme', currentTheme);
+            localStorage.setItem('theme', currentTheme);
+        });
+    }
+
+    toggleTheme() {
+    this.theme.set(this.theme() === 'light' ? 'dark' : 'light');
+  }
+
+    setTheme(theme: 'light' | 'dark'){
+        this.theme.set(theme);
+    }
+
+}
